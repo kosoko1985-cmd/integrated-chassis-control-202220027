a91a9b619b4ab62fe6b1db7cab836240b4e798ca
kosoko1985-cmd <kosoko1985@ajou.ac.kr>
2026-06-23 23:08:23 +0900

    ctrl_lateral_starter.m

--- icc-project/scripts/control/starter/ctrl_lateral_starter.m
+++ icc-project/scripts/control/starter/ctrl_lateral_starter.m
@@ -1,4 +1,19 @@
 function [deltaAdd, ctrlState] = ctrl_lateral(yawRateRef, yawRate, slipAngle, vx, ctrlState, CTRL, LIM, dt)
+    % 입력 구조가 다를 경우를 대비한 유연한 처리 (varargin 대신 struct 확인)
+    % Runner가 만약 첫 번째 인수로 struct 전체를 넘긴다면:
+    if isstruct(yawRateRef)
+        % struct 형태로 데이터가 들어왔을 때 변수 분해
+        in = yawRateRef;
+        yawRate = in.yawRate;
+        slipAngle = in.slipAngle;
+        vx = in.vx;
+        ctrlState = in.ctrlState;
+        CTRL = in.CTRL;
+        LIM = in.LIM;
+        dt = in.dt;
+        yawRateRef = in.yawRateRef; % 마지막에 덮어쓰기
+    end
+
     % 1. 상태 초기화 (적분기)
     if ~isfield(ctrlState, 'intError')
         ctrlState.intError = 0;
@@ -17,6 +32,7 @@ function [deltaAdd, ctrlState] = ctrl_lateral(yawRateRef, yawRate, slipAngle, vx
     speed_factor = min(vx_safe / v_ref, 2); 
     
     steer_req = (CTRL.LAT.Kp * yawError + CTRL.LAT.Ki * ctrlState.intError) / speed_factor;
+    
     deltaAdd.steerAngle = max(-LIM.MAX_STEER_ANGLE, min(LIM.MAX_STEER_ANGLE, steer_req));
 
     % 3. slip angle 제한 (ESC)
